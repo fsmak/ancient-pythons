de9c419aefb8f0ba70c4d48c94a2b910ec5157f4
fsmak <67084504+fsmak@users.noreply.github.com>
2021-06-18 16:56:13 +0800

    Update opcode.h
    增加opcode对应的字符串

--- python-0.9.1/src/opcode.h
+++ python-0.9.1/src/opcode.h
@@ -107,3 +107,134 @@ OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
 enum cmp_op {LT, LE, EQ, NE, GT, GE, IN, NOT_IN, IS, IS_NOT, EXC_MATCH, BAD};
 
 #define HAS_ARG(op) ((op) >= HAVE_ARGUMENT)
+
+static char *opcode_str[128] = {
+    "STOP_CODE",    //0
+    "POP_TOP",      //1
+    "ROT_TWO",      //2
+    "ROT_THREE",    //3
+    "DUP_TOP",      //4
+    "*", //5
+    "*", //6
+    "*", //7
+    "*", //8
+    "*", //9
+    "UNARY_POSITIVE",   // 10
+    "UNARY_NEGATIVE",   // 11
+    "UNARY_NOT",        // 12
+    "UNARY_CONVERT",    // 13
+    "UNARY_CALL",       // 14
+    "*",    //15
+    "*",    //16
+    "*",    //17
+    "*",    //18
+    "*",    //19
+    "BINARY_MULTIPLY",  //  20
+    "BINARY_DIVIDE",    //  21
+    "BINARY_MODULO",    //  22
+    "BINARY_ADD",       //  23
+    "BINARY_SUBTRACT",  //  24
+    "BINARY_SUBSCR",    //  25
+    "BINARY_CALL",      //  26
+    "*",    //27
+    "*",    //28
+    "*",    //29
+    "SLICE",    //          30  /* Also uses 31-33 */
+    "*",        //31
+    "*",        //32
+    "*",        //33
+    "*",        //34
+    "*",        //35
+    "*",        //36
+    "*",        //37
+    "*",        //38
+    "*",        //39
+    "STORE_SLICE",  //    40    /* Also uses 41-43 */
+    "*",        //41
+    "*",        //42
+    "*",        //43
+    "*",        //44
+    "*",        //45
+    "*",        //46
+    "*",        //47
+    "*",        //48
+    "*",        //49
+    "DELETE_SLICE", //   50 /* Also uses 51-53 */
+    "*",        //51
+    "*",        //52
+    "*",        //53
+    "*",        //54
+    "*",        //55
+    "*",        //56
+    "*",        //57
+    "*",        //58
+    "*",        //59
+    "STORE_SUBSCR",     //  60
+    "DELETE_SUBSCR",    //  61
+    "*",        //62
+    "*",        //63
+    "*",        //64
+    "*",        //65
+    "*",        //66
+    "*",        //67
+    "*",        //68
+    "*",        //69
+    "PRINT_EXPR",       //  70
+    "PRINT_ITEM",       //  71
+    "PRINT_NEWLINE",    //  72
+    "*",        //73
+    "*",        //74
+    "*",        //75
+    "*",        //76
+    "*",        //77
+    "*",        //78
+    "*",        //79
+    "BREAK_LOOP",       //     80
+    "RAISE_EXCEPTION",  //     81
+    "LOAD_LOCALS",      //    82
+    "RETURN_VALUE",     //   83
+    "REQUIRE_ARGS",     //   84
+    "REFUSE_ARGS",      //    85
+    "BUILD_FUNCTION",   // 86
+    "POP_BLOCK",        //      87
+    "END_FINALLY",      //    88
+    "BUILD_CLASS",      //    89
+    "STORE_NAME",       //     90      /* Index in name list */
+    "DELETE_NAME",      //    91      /* "" */
+    "UNPACK_TUPLE",     //   92      /* Number of tuple items */
+    "UNPACK_LIST",      //    93      /* Number of list items */
+    "*", /* unused:             94 */
+    "STORE_ATTR",       //     95      /* Index in name list */
+    "DELETE_ATTR",      //    96      /* "" */
+    "*",        //97
+    "*",        //98
+    "*",        //99
+    "LOAD_CONST",       //     100     /* Index in const list */
+    "LOAD_NAME",        //      101     /* Index in name list */
+    "BUILD_TUPLE",      //    102     /* Number of tuple items */
+    "BUILD_LIST",       //     103     /* Number of list items */
+    "BUILD_MAP",        //      104     /* Always zero for now */
+    "LOAD_ATTR",        //      105     /* Index in name list */
+    "COMPARE_OP",       //     106     /* Comparison operator */
+    "IMPORT_NAME",      //    107     /* Index in name list */
+    "IMPORT_FROM",      //    108     /* Index in name list */
+    "*",                //     109
+    "JUMP_FORWARD",     //   110     /* Number of bytes to skip */
+    "JUMP_IF_FALSE",    //  111     /* "" */
+    "JUMP_IF_TRUE",     //   112     /* "" */
+    "JUMP_ABSOLUTE",    //  113     /* Target byte offset from beginning of code */
+    "FOR_LOOP",         //       114     /* Number of bytes to skip */
+    "*",        //115
+    "*",        //116
+    "*",        //117
+    "*",        //118
+    "*",        //119
+    "SETUP_LOOP",       //     120     /* Target address (absolute) */
+    "SETUP_EXCEPT",     //   121     /* "" */
+    "SETUP_FINALLY",    //  122     /* "" */
+    "*",        //123
+    "*",        //124
+    "*",        //125
+    "*",        //126
+    "SET_LINENO",       //     127     /* Current line number */
+    };
